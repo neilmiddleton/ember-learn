a2312547df1f18132585d6c19ae06c4114995c71
Neil Middleton <neil@heroku.com>
2015-01-26 19:31:16 +0000

    Change

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -3,16 +3,16 @@ Todos.TodoController = Ember.ObjectController.extend({
     editToDo: function() {
       this.set('isEditing', false)
     }
-  }
-  // isCompleted: function(key, value) {
-  //   var model = this.get('model');
-  //   if (value === undefined) {
-  //     return this.get('isCompleted');
-  //   } else {
-  //     model.set('isCompleted', value);
-  //     model.save();
-  //     return value;
-  //   }
-  // }.property('model.isCompleted'),
+  },
+  isCompleted: function(key, value) {
+    var model = this.get('model');
+    if (value === undefined) {
+      return this.get('isCompleted');
+    } else {
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted'),
   isEdting: false
 })
